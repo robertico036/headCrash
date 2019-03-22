502b041f4e8fa9e2736b98a96754b5cb9dace270
Robert <robertico036@hotmail.es>
2019-03-21 20:25:22 -0400

    Maurice

---
+++ script.js
@@ -0,0 +1,9 @@
+function hitSalvi() {
+	//hide the anvil
+	var anvil = document.getElementById("anvil");
+	anvil.style.display = "none";
+
+	//change image
+	var salvi = document.getElementById("salvi");
+	salvi.style.backgroundPosition = "-900px -600px"
+}
